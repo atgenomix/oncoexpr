27e73564c024a56da970d0c983ce5beaa4e022ce
charleschuang1993 <charleschuang1993@gmail.com>
2025-03-20 12:01:05 +0800

    feat:  install shinydashboard R package

--- R/RNAseqShinyApp.R
+++ R/RNAseqShinyApp.R
@@ -209,7 +209,8 @@ RNAseqShinyAppSpark <- function() {
     sc <- reactiveVal(NULL)
     
     observe({
-      master <- "sc://172.18.0.1:15002"
+      #master <- "sc://172.18.0.1:15002"
+      master <- "sc://localhost:15002"
       method <- "spark_connect"
       version <- "3.5"
       connection <- sparklyr::spark_connect(master = master, method = method, version = version)
