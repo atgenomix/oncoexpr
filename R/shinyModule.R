e5a909b77db13547db5284674472ed89971486cf
charleschuang1993 <charleschuang1993@gmail.com>
2025-03-24 11:32:46 +0800

    chore(RNAseqShinyApp): update renv settings and clean up .Rhistory for improved package management

--- R/shinyModule.R
+++ R/shinyModule.R
@@ -411,9 +411,9 @@ dbBrowserServer <- function(id, sc) {
     ns <- session$ns
     
     observe({
-      #db_list_query <- dbGetQuery(sc, "SHOW DATABASES")
-      org <- tolower(Sys.getenv("SPARK_USER"))
-      db_list_query <- dbGetQuery(sc, sprintf("SHOW DATABASES LIKE '%%_%s'", org))
+      db_list_query <- dbGetQuery(sc, "SHOW DATABASES")
+      #org <- tolower(Sys.getenv("SPARK_USER"))
+      #db_list_query <- dbGetQuery(sc, sprintf("SHOW DATABASES LIKE '%%_%s'", org))
       db_list <- db_list_query
       
       updateSelectInput(
