167146f7e4ff3d4c20b3f0958232a510d82ddce9
charleschuang1993 <charleschuang1993@gmail.com>
2025-03-29 23:53:41 +0800

    experiment: add print statement to display current future plan in RNAseqShinyApp

--- R/RNAseqShinyApp.R
+++ R/RNAseqShinyApp.R
@@ -57,6 +57,7 @@ NULL
 
 RNAseqShinyAppSpark <- function(master = "sc://172.18.0.1:15002", method = "spark_connect", version = "3.5") {
   plan(multisession, workers =2)
+  print(future::plan())
   ui <- fluidPage(
     navbarPage(
       title = "RNAseq App",
