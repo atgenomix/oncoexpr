06e257011095954a3be33771bf8d2bd7399f452a
charleschuang1993 <charleschuang1993@gmail.com>
2025-04-03 01:49:33 +0800

    chore: update README with installation instructions and usage examples

--- R/DEplotFuns.R
+++ R/DEplotFuns.R
@@ -1,60 +1,58 @@
-
 #' @title limma analysis with MAE input
-#' @description differential expression by limma 
+#' @description differential expression by limma
 #' @param mae multiple assays experiment
-#' @param assayName assay 
+#' @param assayName assay
 #' @param subCodeCol which col for comparasion
-#' @param coef coefficient for limma 
+#' @param coef coefficient for limma
 #' @param pval_cut p-value for cut-off label
 #' @param lfc_cut log fold-change cut-off label
 #' @param useAdjP adjusted p-value
-#' @return limma analysis table 
+#' @return limma analysis table
 #' @export
 
 LimmaMAE <- function(mae,
                      assayName = "RNAseq",
                      # 假設分組資訊在 colData 中的 subCode 欄位
                      subCodeCol = "subCode",
-                     coef = 2,            # 要比較的係數 (group 的哪一層)
+                     coef = 2, # 要比較的係數 (group 的哪一層)
                      pval_cut = 0.05,
-                     lfc_cut  = 1,
-                     useAdjP  = FALSE) {
-  
-  rna_counts  <- assays(mae[[assayName]])$max_TPM
+                     lfc_cut = 1,
+                     useAdjP = FALSE) {
+  rna_counts <- assays(mae[[assayName]])$max_TPM
   sample_info <- colData(mae[[assayName]])
   print(sample_info)
   group <- factor(sample_info[[subCodeCol]])
   print(group)
 
   dge <- DGEList(counts = rna_counts, group = group)
-  dge <- calcNormFactors(dge) 
-  
-  design <- model.matrix(~ group)
-  
+  dge <- calcNormFactors(dge)
+
+  design <- model.matrix(~group)
+
   v <- voom(dge, design)
-  
+
   fit <- lmFit(v, design)
   fit <- eBayes(fit)
-  
+
   dt <- decideTests(fit, p.value = pval_cut, lfc = lfc_cut)
   sumDT <- summary(dt)
-  
+
   topTab <- topTable(fit, coef = coef, number = Inf)
-  
+
   return(list(
     fit            = fit,
     topTable       = topTab,
     decideTests    = dt,
-    upDownSummary  = sumDT  
+    upDownSummary  = sumDT
   ))
 }
 
 #' @title limma analysis with MAE input
-#' @description differential expression by limma 
+#' @description differential expression by limma
 #' @param fit limma fit result
-#' @param assayName assay 
+#' @param assayName assay
 #' @param subCodeCol which col for comparasion
-#' @param coef coefficient for limma 
+#' @param coef coefficient for limma
 #' @param pval_cut p-value for cut-off label
 #' @param lfc_cut log fold-change cut-off label
 #' @param useAdjP adjusted p-value
@@ -62,30 +60,29 @@ LimmaMAE <- function(mae,
 #' @param labelSize gene symbol text size
 #' @param pointSize point plot size
 #' @param topN show a number of genes symbol with high log fold-change
-#' @return limma analysis table 
+#' @return limma analysis table
 #' @export
 
 
 ggvolcano_limma <- function(fit,
-                            coef      = 2,
-                            lfc_cut   = 1,
-                            pval_cut  = 0.05,
-                            useAdjP   = FALSE,
-                            title     = "Volcano Plot",
-                            topN      = 0,
-                            geneCol   = NULL,
-                            pointSize = 2,      
-                            ptAlpha   = 0.6,     
-                            labelSize = 3,       
+                            coef = 2,
+                            lfc_cut = 1,
+                            pval_cut = 0.05,
+                            useAdjP = FALSE,
+                            title = "Volcano Plot",
+                            topN = 0,
+                            geneCol = NULL,
+                            pointSize = 2,
+                            ptAlpha = 0.6,
+                            labelSize = 3,
                             ...) {
-  
   tt <- topTable(
     fit      = fit,
     coef     = coef,
     number   = Inf,
     ...
   )
-  
+
 
   if (is.null(geneCol)) {
     tt$gene <- rownames(tt)
@@ -95,7 +92,7 @@ ggvolcano_limma <- function(fit,
     }
     tt$gene <- tt[[geneCol]]
   }
-  
+
   pvalCol <- if (useAdjP) "adj.P.Val" else "P.Value"
   if (!pvalCol %in% colnames(tt)) {
     stop(paste0("無法在 topTable 結果中找到欄位: ", pvalCol))
@@ -106,9 +103,9 @@ ggvolcano_limma <- function(fit,
     logFC = tt$logFC,
     pval  = tt[[pvalCol]]
   )
-  
+
   plotData$color <- "grey"
-  plotData$color[plotData$logFC >=  lfc_cut & plotData$pval <= pval_cut] <- "red"
+  plotData$color[plotData$logFC >= lfc_cut & plotData$pval <= pval_cut] <- "red"
   plotData$color[plotData$logFC <= -lfc_cut & plotData$pval <= pval_cut] <- "blue"
 
   plotData$negLogP <- -log10(plotData$pval)
@@ -121,13 +118,13 @@ ggvolcano_limma <- function(fit,
     theme_classic() +
     labs(
       title = title,
-      x     = expression(log[2]~"Fold Change"),
-      y     = bquote(-log[10]~.(pvalCol))
+      x     = expression(log[2] ~ "Fold Change"),
+      y     = bquote(-log[10] ~ .(pvalCol))
     )
-  
+
   if (topN > 0) {
     topGenesDF <- head(plotData[order(plotData$pval), ], n = topN)
-    p <- p + 
+    p <- p +
       ggrepel::geom_text_repel(
         data = topGenesDF,
         aes(label = gene),
@@ -135,7 +132,7 @@ ggvolcano_limma <- function(fit,
         max.overlaps = Inf
       )
   }
-  
+
   return(p)
 }
 
@@ -147,7 +144,7 @@ ggvolcano_limma <- function(fit,
 #' @param geneName gene symbol
 #' @param pValCol which col for p-value
 #' @param logFCCol which col for log fold-change
-#' @param coef coefficient for limma 
+#' @param coef coefficient for limma
 #' @param pval_cut p-value for cut-off label
 #' @param lfc_cut log fold-change cut-off label
 #' @param useAdjP adjusted p-value
@@ -161,31 +158,44 @@ ggvolcano_limma <- function(fit,
 #' @export
 
 
-ggvolcano_custom <- function (df, geneName, pValCol = "PValue", logFCCol = "logFC", 
-    coef = 2, lfc_cut = 1, pval_cut = 0.05, useAdjP = FALSE, 
-    title = "Volcano Plot", topN = 20, geneCol = NULL, pointSize = 2, 
+ggvolcano_custom <- function(
+    df, geneName, pValCol = "PValue", logFCCol = "logFC",
+    coef = 2, lfc_cut = 1, pval_cut = 0.05, useAdjP = FALSE,
+    title = "Volcano Plot", topN = 20, geneCol = NULL, pointSize = 2,
     ptAlpha = 0.6, labelSize = 3) {
-    plotData <- data.frame(gene = geneName, logFC = df[[logFCCol]], pval = df[[pValCol]])
-    plotData$color <- "grey"
-    plotData$color[plotData$logFC >= lfc_cut & plotData$pval <= 
-        pval_cut] <- "red"
-    plotData$color[plotData$logFC <= -lfc_cut & plotData$pval <= 
-        pval_cut] <- "blue"
-    plotData$negLogP <- -log10(plotData$pval)
-    p <- ggplot(plotData, aes(x = logFC, y = negLogP, color = color)) + 
-        geom_point(size = pointSize, alpha = ptAlpha) + scale_color_identity() + 
-        geom_vline(xintercept = c(-lfc_cut, lfc_cut), linetype = "dashed", 
-            color = "black") + geom_hline(yintercept = -log10(pval_cut), 
-        linetype = "dashed", color = "black") + theme_classic() + 
-        labs(title = title, x = expression(log[2] ~ "Fold Change"), 
-            y = bquote(-log[10] ~ .(pValCol)))
-    if (topN > 0) {
-        topGenesDF <- head(plotData[order(plotData$pval), ], 
-            n = topN)
-        p <- p + ggrepel::geom_text_repel(data = topGenesDF, 
-            aes(label = gene), size = labelSize, max.overlaps = Inf)
-    }
-    return(p)
+  plotData <- data.frame(gene = geneName, logFC = df[[logFCCol]], pval = df[[pValCol]])
+  plotData$color <- "grey"
+  plotData$color[plotData$logFC >= lfc_cut & plotData$pval <=
+    pval_cut] <- "red"
+  plotData$color[plotData$logFC <= -lfc_cut & plotData$pval <=
+    pval_cut] <- "blue"
+  plotData$negLogP <- -log10(plotData$pval)
+  p <- ggplot(plotData, aes(x = logFC, y = negLogP, color = color)) +
+    geom_point(size = pointSize, alpha = ptAlpha) +
+    scale_color_identity() +
+    geom_vline(
+      xintercept = c(-lfc_cut, lfc_cut), linetype = "dashed",
+      color = "black"
+    ) +
+    geom_hline(
+      yintercept = -log10(pval_cut),
+      linetype = "dashed", color = "black"
+    ) +
+    theme_classic() +
+    labs(
+      title = title, x = expression(log[2] ~ "Fold Change"),
+      y = bquote(-log[10] ~ .(pValCol))
+    )
+  if (topN > 0) {
+    topGenesDF <- head(plotData[order(plotData$pval), ],
+      n = topN
+    )
+    p <- p + ggrepel::geom_text_repel(
+      data = topGenesDF,
+      aes(label = gene), size = labelSize, max.overlaps = Inf
+    )
+  }
+  return(p)
 }
 
 #' @title ggvolcano_custom_interactive
@@ -194,7 +204,7 @@ ggvolcano_custom <- function (df, geneName, pValCol = "PValue", logFCCol = "logF
 #' @param geneName gene symbol
 #' @param pValCol which col for p-value
 #' @param logFCCol which col for log fold-change
-#' @param coef coefficient for limma 
+#' @param coef coefficient for limma
 #' @param pval_cut p-value for cut-off label
 #' @param lfc_cut log fold-change cut-off label
 #' @param useAdjP adjusted p-value
@@ -208,49 +218,53 @@ ggvolcano_custom <- function (df, geneName, pValCol = "PValue", logFCCol = "logF
 #' @return ggplot object
 #' @export
 
-ggvolcano_custom_interactive <- function(df, geneName, pValCol = "PValue", logFCCol = "logFC", 
-                                           coef = 2, lfc_cut = 1, pval_cut = 0.05, useAdjP = FALSE, 
-                                           title = "Volcano Plot", topN = 20, geneCol = NULL, 
-                                           pointSize = 2, ptAlpha = 0.6, labelSize = 3,
-                                           highlight = NULL) { 
-  plotData <- data.frame(gene = geneName, 
-                         logFC = df[[logFCCol]], 
-                         pval = df[[pValCol]])
-  
+ggvolcano_custom_interactive <- function(df, geneName, pValCol = "PValue", logFCCol = "logFC",
+                                         coef = 2, lfc_cut = 1, pval_cut = 0.05, useAdjP = FALSE,
+                                         title = "Volcano Plot", topN = 20, geneCol = NULL,
+                                         pointSize = 2, ptAlpha = 0.6, labelSize = 3,
+                                         highlight = NULL) {
+  plotData <- data.frame(
+    gene = geneName,
+    logFC = df[[logFCCol]],
+    pval = df[[pValCol]]
+  )
+
   plotData$negLogP <- -log10(plotData$pval)
-  
+
   plotData$color <- "grey"
   plotData$color[plotData$logFC >= lfc_cut & plotData$pval <= pval_cut] <- "red"
   plotData$color[plotData$logFC <= -lfc_cut & plotData$pval <= pval_cut] <- "blue"
-  
+
   if (!is.null(highlight)) {
     plotData$color[plotData$gene %in% highlight] <- "orange"
   }
-  
+
   p <- ggplot(plotData, aes(x = logFC, y = negLogP, color = color)) +
-    ggiraph::geom_point_interactive(aes(tooltip = gene, data_id = gene), 
-                                     size = pointSize, alpha = ptAlpha) +
+    ggiraph::geom_point_interactive(aes(tooltip = gene, data_id = gene),
+      size = pointSize, alpha = ptAlpha
+    ) +
     scale_color_identity() +
     geom_vline(xintercept = c(-lfc_cut, lfc_cut), linetype = "dashed", color = "black") +
     geom_hline(yintercept = -log10(pval_cut), linetype = "dashed", color = "black") +
     theme_classic() +
-    labs(title = title, 
-         x = expression(log[2] ~ "Fold Change"), 
-         y = bquote(-log[10] ~ .(pValCol)))
-  
+    labs(
+      title = title,
+      x = expression(log[2] ~ "Fold Change"),
+      y = bquote(-log[10] ~ .(pValCol))
+    )
+
   if (topN > 0 && nrow(plotData) > 0) {
     topGenesDF <- head(plotData[order(plotData$pval), ], n = topN)
-    p <- p + ggrepel::geom_text_repel(data = topGenesDF, 
-                                      aes(label = gene), 
-                                      size = labelSize, 
-                                      max.overlaps = Inf)
+    p <- p + ggrepel::geom_text_repel(
+      data = topGenesDF,
+      aes(label = gene),
+      size = labelSize,
+      max.overlaps = Inf
+    )
   }
-  
+
   return(p)
 }
-
-
-
 
 #' @title PCAplot
 #' @description PCA plot for RNAseq expression
@@ -260,27 +274,28 @@ ggvolcano_custom_interactive <- function(df, geneName, pValCol = "PValue", logFC
 #' @param pcY y axis PC dimension
 #' @return PCA plot
 #' @export
-#' 
+#'
 createPCAPlot <- function(pcaResult, colData, pcX, pcY) {
   # Convert PCA results to data frame
   pcaData <- as.data.frame(pcaResult$x)
   pcaData$Sample <- rownames(pcaData)
-  
+
   # Standardize sample names by replacing dots with dashes
   pcaData$Sample <- gsub("\\.", "-", pcaData$Sample)
-  
+
   # Merge PCA results with colData based on sample IDs
   mergedData <- merge(pcaData, colData, by.x = "Sample", by.y = "mainCode", all.x = TRUE)
   missing <- mergedData[is.na(mergedData$subCode), "Sample"]
-  if(length(missing) > 0) {
+  if (length(missing) > 0) {
     message("Missing group annotation for samples: ", paste(missing, collapse = ", "))
   }
   # Create the PCA plot with ggplot2, mapping color to the group (subCode)
   ggplot(mergedData, aes_string(x = pcX, y = pcY, label = "Sample", color = "subCode")) +
     geom_point(size = 3) +
     geom_text(vjust = -0.5) +
-    labs(title = paste("PCA Plot:", pcX, "vs", pcY),
-         x = pcX, y = pcY, color = "Group") +
+    labs(
+      title = paste("PCA Plot:", pcX, "vs", pcY),
+      x = pcX, y = pcY, color = "Group"
+    ) +
     theme_minimal()
 }
-
