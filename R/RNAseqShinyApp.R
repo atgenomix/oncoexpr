8d95358c4ea29c67a3077210986659a5793a19a8
charleschuang1993 <charleschuang1993@gmail.com>
2025-04-08 12:13:30 +0800

    feat: add print statement for selected_db_name in RNAseqShinyApp

--- R/RNAseqShinyApp.R
+++ R/RNAseqShinyApp.R
@@ -252,6 +252,7 @@ RNAseqShinyAppSpark <- function(master = "sc://172.18.0.1:15002", method = "spar
       #selected_db_name <- results$db_info$selected_db()
       print("select db")
       selected_db_name <- "0408_b202406002_tvsnt_org_hhtwxpybru3ligh"
+      print(selected_db_name)
       a_ <- future_promise(
         {
           sc_conn <- sparklyr::spark_connect(master = master, method = method, version = version)
