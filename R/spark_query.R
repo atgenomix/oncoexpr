908e18106700e5368b122536fa6ad05c6cdcac50
charleschuang1993 <charleschuang1993@gmail.com>
2025-04-16 16:30:18 +0800

    feat: add functions for converting data formats and querying Spark datasets

--- R/spark_query.R
+++ R/spark_query.R
@@ -1,54 +1,3 @@
-
-#' @title se to longformat
-#' @description se from MAE package transfered to long format dataframe
-#' @param se_ se object 
-#' @param assay_name selecting assay from se object
-#' @param selected_row_ select gene
-#' @param selected_col_ select sample
-#' @param selected_colData_ filter colData column
-#' @return pivot long format from se object
-#' @export
-#' 
-se2longformat <- function(se_ = exp_brca, 
-                          assay_name = "fpkm_unstrand", 
-                          selected_row_ = c(1:5), 
-                          selected_col_ = c("TCGA-5L-AAT0-01A-12R-A41B-07", "TCGA-A2-A04U-01A-11R-A115-07"), 
-                          selected_colData_ = "tissue_type") {
-  
-  assay_mat <- assay(se_, assay_name)
-  
-  if (is.null(colnames(assay_mat))) {
-    stop("Assay matrix does not have column names!")
-  }
-  
-  new_colData <- colData(se_)
-  rownames(new_colData) <- colnames(se_)
-  
-  se_new <- SummarizedExperiment(
-    assays = list(data = assay_mat),
-    colData = new_colData,
-    rowRanges = rowRanges(se_)
-  )
-  
-  mae_new <- MultiAssayExperiment(setNames(list(se_new), assay_name))
-  colData(mae_new) <- colData(se_new)
-  if (is.null(selected_row_)) {
-    selected_row_ <- rownames(se_new)
-  } else if (is.numeric(selected_row_)) {
-    selected_row_ <- rownames(se_new)[selected_row_]
-  }
-  if (is.null(selected_col_)) {
-    selected_col_ <- seq_len(ncol(se_new))
-  }
-  
-  subset_mae <- mae_new[selected_row_, selected_col_, assay_name]
-  
-  se2longformat_ <- longFormat(subset_mae, colDataCols = selected_colData_)
-  
-  return(se2longformat_)
-}
-
-
 #' @title view spark
 #' @description show spark datasets and tables
 #' @param sc spark connection
@@ -90,50 +39,6 @@ query_spark_rnaseqdata <- function(sc, use_table, mainCode_ = "BC026", subCode_ 
   list_return <- list ("mainCode" = mainCode_, "subCode" = subCode_, "normMethod" = normMethod,  "data" = df_filtering_local )
   return(list_return)
 }
-
-
-#' @title generate expression matrix with multiple samples
-#' @description convert long to wide for SQL dataframe from spark database
-#' @param data fileterd data from spark database 
-#' @param mainCode_ mainCode selection
-#' @param subCode_ subCode selection
-#' @return a expression matrix with gene row and sample column
-#' @export
-#' 
-convert_long_to_wide <- function(data, mainCode_, subCode_) {
-  data <- data %>%
-    mutate(sample_name = paste(!!sym(mainCode_), !!sym(subCode_), sep = "_"))
-  
-  rna_expr <- data %>%
-    select(Gene, sample_name, value) %>%
-    pivot_wider(names_from = sample_name, values_from = value, values_fill = NULL)
-  
-  return(rna_expr)
-}
-
-#' @title sample information table
-#' @description generate sample information table for MAE colData 
-#' @param data fileterd data from spark database 
-#' @param mainCode_ mainCode selection
-#' @param subCode_ subCode selection
-#' @return a table about sample codes
-#' @export
-#' 
-generate_sample_info <- function(data, mainCode_, subCode_) {
-  # 提取 mainCode 和 subCode 並生成樣本代號
-  sample_info <- data %>%
-    distinct(!!sym(mainCode_), !!sym(subCode_)) %>%
-    mutate(sample_id = paste0("S", sprintf("%03d", row_number())))
-  
-  # 整理樣本資訊表格
-  sample_info_table <- sample_info %>%
-    select(sample_id, !!sym(mainCode_), !!sym(subCode_))
-  
-  return(sample_info_table)
-}
-
-
-
 
 #' Trigger a Spark Query by Table Name Pattern
 #'
