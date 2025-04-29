4a1e8deee46d062e105c95b3c75c1e4a8be68c4c
charleschuang1993 <charleschuang1993@gmail.com>
2025-04-29 16:25:29 +0800

    fix: remove scoreType parameter from gseKEGG function calls

--- R/mod_enrichment.R
+++ R/mod_enrichment.R
@@ -52,7 +52,6 @@ gseaFCModuleServer <- function(id, DEG_table, direction = c("up", "down")) {
           gsea_res <- gseKEGG(
             geneList = geneList,
             organism = "hsa",
-            scoreType = "pos",
             minGSSize = 10,
             maxGSSize = 500,
             pvalueCutoff = local_pvalCutoff,
@@ -84,7 +83,7 @@ gseaFCModuleServer <- function(id, DEG_table, direction = c("up", "down")) {
           gsea_res <- gseKEGG(
             geneList = geneList,
             organism = "hsa",
-            scoreType = "pos",
+            
             minGSSize = 10,
             maxGSSize = 500,
             pvalueCutoff = local_pvalCutoff,
